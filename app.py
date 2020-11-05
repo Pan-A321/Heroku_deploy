e3ebddc8515e5ff612c5a5076ea8cfc3899ca04f
unknown <Pan>
2020-11-05 11:15:09 +0800

    make it better

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-
-#import tensorflow as tf 
-from PIL import Image
-#import matplotlib.pyplot as plt
-import librosa
-import librosa.display
-import numpy as np
-import os
 from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
         
